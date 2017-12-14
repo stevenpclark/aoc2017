a0c7d5ee77a2eef5d80d642ee6c1375d5b473075
Steven Clark <steven.p.clark@gmail.com>
2017-12-13 22:47:30 -0500

    after some headbanging

---
+++ 13.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+class Scanner(object):
+    def __init__(self, depth, span):
+        self.depth = depth
+        self.span = span
+        self._period = 2*(span-1)
+
+    def at_zero(self, t):
+        return t%self._period == 0
+
+
+def get_min_safe_delay(scanners):
+    delay = 0
+    while True:
+        #if delay % 1000 == 0:
+            #print delay
+        for scanner in scanners:
+            if scanner.at_zero(delay+scanner.depth):
+                #caught!
+                break
+        else:
+            #made it all the way through!
+            return delay
+        delay += 1
+
+
+if __name__ == '__main__':
+    depths = []
+    spans = []
+    with open('13.txt', 'r') as f:
+        for li in f.readlines():
+            s_depth, s_span = li.split(':')
+            depths.append(int(s_depth))
+            spans.append(int(s_span))
+    #depths = [0,1,4,6]
+    #spans = [3,2,4,4]
+
+    scanners = [Scanner(d,s) for d,s in zip(depths, spans)]
+
+    print(get_min_safe_delay(scanners))
+
+
+
