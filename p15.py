6121b32b4be3255dbaa92bae499aff29e9ffb134
Steven Clark <steven.p.clark@gmail.com>
2017-12-15 21:21:09 -0500

    15

---
+++ p15.py
@@ -0,0 +1,22 @@
+def my_gen(start, factor):
+    prev = start
+    while True:
+        prev = (prev*factor)%2147483647
+        yield prev
+
+if __name__ == '__main__':
+    #genA = my_gen(65, 16807)
+    #genB = my_gen(8921, 48271)
+    #print(genA.next())
+    #print(genA.next())
+
+    genA = my_gen(703, 16807)
+    genB = my_gen(516, 48271)
+
+    count = 0
+    for i in range(40000000):
+        valA = genA.next()
+        valB = genB.next()
+        if (valA & 0xFFFF) == (valB & 0xFFFF):
+            count += 1
+    print(count)
